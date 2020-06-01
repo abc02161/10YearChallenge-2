80fbbb507116b01bcf0d382879f88fcffabeb3b6
Nick <abc02161@yahoo.com.tw>
2020-06-01 16:20:49 +0800

    Initial Commit

---
+++ 10YearChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10YearChallenge
+//
+//  Created by Nick on 2020/6/1.
+//  Copyright © 2020 NewIdea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
